4ac1f9ae634c52cc457b64f5d661ecbf2cf75f67
Massyknight <arunrobert1998@gmail.com>
2021-09-16 23:40:42 -0700

    add user model and connect to db

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const mongoose= require('mongoose');
 const bodyParser= require('body-parser');
 const port=8000;
 const app= express();
+const User = require('./models/User'); // makes our User model available to use in express routes.
+mongoose.connect('mongodb://localhost/userData') //connects us to a local mongoDB database called: userData.
 
 app.use(bodyParser.json());
 
@@ -10,10 +12,18 @@ app.listen(port, ()=>{
 	console.log(`server is listening on port:${port}`)
 })
 
+
+//Inside each (req,res) callback function we use mongoose methods on our User model to 
+//Create, Read, Update, and Delete individual user documents in our users collection. 
+//The "POST" route is different than the others because mongoDB automatically creates an ID for each document when it is created.
+
 // CREATE
 app.post('/users',(req,res)=>{
   // User.create()
 })
+
+// We are using route chaining as a shorthand for the "GET", "PUT", and "DELETE" routes, 
+//since they all use the /users/:id endpoint. The :id part of the endpoint is a variable which can be accessed in the "req.params"
 
 app.route('/users/:id')
 // READ
